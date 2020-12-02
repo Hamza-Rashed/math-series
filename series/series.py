e52d404fcefd249987778a9c95f0f0aca1a90aab
Hamza-Rashed <62372964+Hamza-Rashed@users.noreply.github.com>
2020-12-02 10:13:11 +0200

    Update series.py

--- series/series.py
+++ series/series.py
@@ -1,5 +1,9 @@
 # function fibonacci
 def fibonacci(n):
+    """
+    is a numeric series starting with the integers 0 and 1. In this series, 
+    the next integer is determined by summing the previous two. This gives us:
+    """
     if n == 1:
         return 1
     if n == 0:
@@ -8,6 +12,10 @@ def fibonacci(n):
 
 # function lucas
 def lucas(n):
+    """
+    The Lucas Numbers are a related series of integers that start with 
+    the values 2 and 1 rather than 0 and 1. The resulting series looks like this:
+    """
     if n == 1:
         return 1
     if n == 0:
@@ -16,6 +24,13 @@ def lucas(n):
 
 # function sum_series
 def sum_series(n,first_num=0,second_num=1):
+
+    """
+   this function take one required parameter and two optional parameters.
+   The required parameter will determine which element in the series to print. 
+   The two optional parameters will have default values of 0 and 1 and will 
+   determine the first two values for the series to be produced.
+    """
 
     if first_num == 2 and second_num == 1:
         return lucas(n)
